4e32544f32f68408eb02f691cfd38ef3968c7f6f
dieloren <tdiegoldc@gmail.com>
2023-11-16 19:15:21 +0100

    Agregados las funciones ft_substr y ft_strjoin

---
+++ Libft/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieloren <dieloren@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 17:55:29 by dieloren          #+#    #+#             */
+/*   Updated: 2023/11/15 17:55:29 by dieloren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	j = 0;
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	while (s1[j])
+	{
+		str[i] = s1[j];
+		i++;
+		j++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		str[i] = s2[j];
+		i++;
+		j++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ Libft/ft_substr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieloren <dieloren@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 17:33:11 by dieloren          #+#    #+#             */
+/*   Updated: 2023/11/15 17:33:11 by dieloren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	j;
+
+	i = start;
+	j = 0;
+	if (ft_strlen(s) == 0 || len <= 0 || start < 0 || start > ft_strlen(s))
+		return ((char *)ft_calloc(1, 1));
+	while (s[i] && j < len)
+	{
+		i++;
+		j++;
+	}
+	substr = (char *)malloc((j + 1) * sizeof(char));
+	if (substr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < j)
+	{
+		substr[i] = s[start];
+		i++;
+		start++;
+	}
+	substr[i] = '\0';
+	return (substr);
+}
