8e8994681c2eb0f08fed5943629e3bc02af4b7f7
dieloren <tdiegoldc@gmail.com>
2024-01-16 13:49:12 +0100

    .

---
+++ printf/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dieloren <dieloren@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 23:03:11 by dieloren          #+#    #+#             */
+/*   Updated: 2024/01/15 23:03:11 by dieloren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <string.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+#endif
