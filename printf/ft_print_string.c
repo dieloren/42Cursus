4effc6e6347b3d1f7979f6e52f6187172d942d05
Diego Lorente Del Castillo <dieloren@c2r5s1.42malaga.com>
2024-01-17 14:14:58 +0100

    .

--- printf/ft_print_string.c
+++ printf/ft_print_string.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_print_string.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dieloren <dieloren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/15 23:03:11 by dieloren          #+#    #+#             */
-/*   Updated: 2024/01/17 14:14:23 by dieloren         ###   ########.fr       */
+/*   Created: 2024/01/17 14:13:31 by dieloren          #+#    #+#             */
+/*   Updated: 2024/01/17 14:14:11 by dieloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-
-# include <stdio.h>
-# include <unistd.h>
-# include <string.h>
-# include <stdlib.h>
-# include <stdarg.h>
-
-int ft_printf(const char *format, ...);
-int	ft_check_character(const char *format, int i, va_list args);
-int	ft_print_string(char *str);
-
-#endif
+int	ft_print_string(char *str)
+{
+	
+}
--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,17 +6,53 @@
 /*   By: dieloren <dieloren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 00:42:58 by dieloren          #+#    #+#             */
-/*   Updated: 2024/01/16 00:42:58 by dieloren         ###   ########.fr       */
+/*   Updated: 2024/01/17 14:12:34 by dieloren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		ft_printf(const char *format, ...)
+int	ft_check_character(const char *format, int i, va_list args)
 {
-	va_list	ap;
+	int	printed_chars;
+
+	printed_chars = 0;
+	if (format[i + 1] == '%')
+	{
+		write(1, '%', 1);
+		return (1);
+	}
+	else if (format[i + 1] == 'c')
+	{
+		write(1, va_arg(args, int), 1);
+		return (1);
+	}
+	else if (format[i + 1] == 's')
+	{
+		
+	}
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
 	int		i;
 	int		printed_chars;
 
-	
+	i = 0;
+	printed_chars = 0;
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			printed_chars += ft_check_character(format, i, args);
+		}
+		else
+		{
+			write(1, &format[i], 1);
+			printed_chars++;
+		}
+		i++;
+	}
 }
